b3161962e8cbd972255f1ddcaf90cfce9574c9c1
Ofek Atar <ofekatar98@gmail.com>
2021-06-06 21:50:20 +0300

    Added cluster workers.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,9 @@
-import serverConfig from "@configs/server.config";
 import createApp from "@app";
+import serverConfig from "@configs/server.config";
 import logger from "@helpers/logs/logger";
+import cluster from "cluster";
 import http from "http";
+import os from "os";
 
 
 async function startServer() {
@@ -9,9 +11,17 @@ async function startServer() {
     const app = await createApp();
     const server = http.createServer(app);
 
-    return server.listen(port, () => {
-        logger.info(`Server started.\nListening on port ${port}...`);
-    });
+    if (cluster.isMaster) {
+        const NUM_OF_FORKS = os.cpus().length;
+        for (let i = 0; i < NUM_OF_FORKS; i++) {
+            cluster.fork();
+        }
+        return;
+    } else {
+        return server.listen(port, () => {
+            logger.info(`Server started.\nListening on port ${port}...`);
+        });
+    }
 }
 
 export default startServer();
