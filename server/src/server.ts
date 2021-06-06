9046c97ac4138192ae942cae01d067e3f05bbe05
Ofek Atar <ofekatar98@gmail.com>
2021-06-06 22:32:09 +0300

    Added pm2.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,7 @@
 import createApp from "@app";
 import serverConfig from "@configs/server.config";
 import logger from "@helpers/logs/logger";
-import cluster from "cluster";
 import http from "http";
-import os from "os";
 
 
 async function startServer() {
@@ -11,17 +9,9 @@ async function startServer() {
     const app = await createApp();
     const server = http.createServer(app);
 
-    if (cluster.isMaster) {
-        const NUM_OF_FORKS = os.cpus().length;
-        for (let i = 0; i < NUM_OF_FORKS; i++) {
-            cluster.fork();
-        }
-        return;
-    } else {
-        return server.listen(port, () => {
-            logger.info(`Server started.\nListening on port ${port}...`);
-        });
-    }
+    return server.listen(port, () => {
+        logger.info(`Server started.\nListening on port ${port}...`);
+    });
 }
 
 export default startServer();
