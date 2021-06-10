32f9eab25b5e55727145898a9401dbe44a581f7e
Ofek Atar <ofekatar98@gmail.com>
2021-06-10 10:42:46 +0300

    Created launch mongo entity.

---
+++ server/src/launches/launches.mongo.ts
@@ -0,0 +1,40 @@
+import { model, Schema } from "mongoose";
+
+export default model(
+    'Launch',
+    new Schema(
+        {
+            flightNumber: {
+                type: Number,
+                required: true,
+            },
+            target: {
+                type: String,
+                required: true,
+            },
+            launchDate: {
+                type: Date,
+                required: true,
+            },
+            mission: {
+                type: String,
+                required: true,
+            },
+            rocket: {
+                type: String,
+                required: true,
+            },
+            upcoming: {
+                type: Boolean,
+                required: true,
+                default: true,
+            },
+            success: {
+                type: Boolean,
+                required: true,
+                default: true,
+            },
+            customers: [String],
+        }
+    )
+)
