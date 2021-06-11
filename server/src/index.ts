f6b0a9cb2575871e9336e202992a3c467090cc5a
Ofek Atar <ofekatar98@gmail.com>
2021-06-12 00:10:18 +0300

    Added environment log.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import "reflect-metadata";
+import config from "@shared/config";
 import { loadDbConnection } from "@core/infra/data/db";
 import startServer from "@core/infra/http/server";
 import { loadModulesData } from "index";
+import logger from "@core/infra/logs/logger";
 
 async function main() {
+    logger.info(`Environment: ${config.nodeEnv}`);
     await loadDbConnection();
     await loadModulesData();
     startServer();
